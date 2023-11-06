eaf726b9c4b02a6866225c91af403490fdb2aadc
JoAraEsp <213345@ids.upchiapas.edu.mx>
2023-11-06 12:38:49 -0600

    changes

--- argon.py
+++ argon.py
@@ -7,6 +7,8 @@ class ArgonParser:
         self.entrada = entrada.strip()
         self.indice = 0
         self.error = None
+        self.funciones_declaradas = set()
+        self.variables_declaradas = set()
 
     def consumir_espacios(self):
         while self.indice < len(self.entrada) and self.entrada[self.indice].isspace():
@@ -24,6 +26,10 @@ class ArgonParser:
         match_obj = self.match(r'(int|float|bool|string|char):[a-zA-Z][a-zA-Z0-9]*=')
         if match_obj:
             tipo, nombre = re.match(r'(int|float|bool|string|char):([a-zA-Z][a-zA-Z0-9]*)=', match_obj).groups()
+            if nombre in self.variables_declaradas:
+                self.error = f'Error: la variable "{nombre}" ya está declarada'
+                return False
+            self.variables_declaradas.add(nombre)
             if tipo == 'int':
                 valor = self.match(r'\d+')
             elif tipo == 'float':
@@ -42,27 +48,36 @@ class ArgonParser:
         else:
             self.error = f'Error en la declaración de variable en la posición {self.indice}'
             return False
-
+        
     def funcion(self):
-        if self.match(r'function'):
+        if self.match(r'fn'):
             nombre_funcion = self.match(r'[a-zA-Z][a-zA-Z0-9]*')
             if nombre_funcion:
+                if nombre_funcion in self.funciones_declaradas:
+                    self.error = f'Error: la función "{nombre_funcion}" ya está declarada'
+                    return False
+                self.funciones_declaradas.add(nombre_funcion)
                 if self.match(r'\('):
-                    if self.match(r'\)'):
-                        if self.match(r':'):
-                            if self.match(r'\{'):
-                                while self.contenido():
-                                    pass
-                                if self.match(r'\}'):
-                                    return True
+                    parametros = self.match(r'([a-zA-Z][a-zA-Z0-9]*(, [a-zA-Z][a-zA-Z0-9]*)*)?\)')
+                    if parametros:
+                        parametros = parametros[:-1]  
+                        lista_parametros = parametros.split(', ')
+                    if self.match(r':'):
+                        if self.match(r'\{'):
+                            while self.contenido():
+                                pass
+                            if self.match(r'\}'):
+                                for param in lista_parametros:
+                                    self.variables_declaradas.discard(param)
+                                return True
                             else:
-                                self.error = 'Error: se esperaba una llave de apertura para el cuerpo de la función'
+                                self.error = 'Error: se esperaba una llave de cierre para el cuerpo de la función'
                                 return False
                         else:
-                            self.error = f'Error: falta el signo dos puntos en la definición de la función {nombre_funcion}'
+                            self.error = 'Error: se esperaba una llave de apertura para el cuerpo de la función'
                             return False
                     else:
-                        self.error = 'Error: se esperaba un signo paréntesis de cierre luego del nombre de la función'
+                        self.error = f'Error: falta el signo dos puntos en la definición de la función {nombre_funcion}'
                         return False
                 else:
                     self.error = 'Error: se esperaba un signo paréntesis de apertura luego del nombre de la función'
@@ -72,6 +87,7 @@ class ArgonParser:
                 return False
         else:
             return False
+
 
     def ciclo(self):
         if self.match(r'loop'):
@@ -112,23 +128,20 @@ class ArgonParser:
                         while self.contenido():
                             pass
                         if self.match(r'\}'):
+                            self.consumir_espacios()
                             if self.elif_condicional():
-                                return True
-                            elif self.match(r'otherwise'):
-                                if self.match(r':'):
-                                    if self.match(r'\{'):
-                                        while self.contenido():
-                                            pass
-                                        if self.match(r'\}'):
-                                            return True
-                                    else:
-                                        self.error = 'Error: se esperaba una llave de apertura para el cuerpo del bloque "otherwise"'
-                                        return False
+                                self.consumir_espacios()
+                                if self.match(r'otherwise'):
+                                    return self.otherwise_bloque()
                                 else:
-                                    self.error = 'Error: se esperaba un signo dos puntos después del bloque "otherwise"'
-                                    return False
+                                    return True
+                            elif self.match(r'otherwise'):
+                                return self.otherwise_bloque()
                             else:
                                 return True
+                        else:
+                            self.error = 'Error: se esperaba una llave de cierre para el cuerpo del bloque "assuming"'
+                            return False
                     else:
                         self.error = 'Error: se esperaba una llave de apertura para el cuerpo del bloque "assuming"'
                         return False
@@ -142,31 +155,19 @@ class ArgonParser:
             return False
 
     def elif_condicional(self):
-        if self.match(r'elif'):
+
+        dentro_de_elif = False
+        while self.match(r'elif'):
+            dentro_de_elif = True
             condicion = self.comparacion()
             if condicion:
                 if self.match(r':'):
                     if self.match(r'\{'):
                         while self.contenido():
                             pass
-                        if self.match(r'\}'):
-                            if self.elif_condicional():
-                                return True
-                            elif self.match(r'otherwise'):
-                                if self.match(r':'):
-                                    if self.match(r'\{'):
-                                        while self.contenido():
-                                            pass
-                                        if self.match(r'\}'):
-                                            return True
-                                    else:
-                                        self.error = 'Error: se esperaba una llave de apertura para el cuerpo del bloque "otherwise"'
-                                        return False
-                                else:
-                                    self.error = 'Error: se esperaba un signo dos puntos después del bloque "otherwise"'
-                                    return False
-                            else:
-                                return True
+                        if not self.match(r'\}'):
+                            self.error = 'Error: se esperaba una llave de cierre para el cuerpo del bloque "elif"'
+                            return False
                     else:
                         self.error = 'Error: se esperaba una llave de apertura para el cuerpo del bloque "elif"'
                         return False
@@ -176,7 +177,28 @@ class ArgonParser:
             else:
                 self.error = 'Error: se esperaba una comparación para la condición del bloque "elif"'
                 return False
+            
+            self.consumir_espacios()
+
+        if dentro_de_elif and self.match(r'otherwise'):
+            return self.otherwise_bloque()
+        
+        return dentro_de_elif
+
+    def otherwise_bloque(self):
+        if self.match(r':'):
+            if self.match(r'\{'):
+                while self.contenido():
+                    pass
+                if not self.match(r'\}'):
+                    self.error = 'Error: se esperaba una llave de cierre para el cuerpo del bloque "otherwise"'
+                    return False
+                return True
+            else:
+                self.error = 'Error: se esperaba una llave de apertura para el cuerpo del bloque "otherwise"'
+                return False
         else:
+            self.error = 'Error: se esperaba un signo dos puntos después del bloque "otherwise"'
             return False
 
     def contenido(self):
@@ -184,13 +206,16 @@ class ArgonParser:
 
     def comparacion(self):
         operadores = r'(==|!=|<=|>=|<|>)'
+        self.consumir_espacios()
         izquierda = self.match(r'[a-zA-Z][a-zA-Z0-9]*')
         if izquierda:
+            self.consumir_espacios()
             resto_izquierda = self.match(r'[a-zA-Z0-9]*')
             while resto_izquierda:
                 izquierda += resto_izquierda
                 resto_izquierda = self.match(r'[a-zA-Z0-9]*')
             if self.match(operadores):
+                self.consumir_espacios()
                 derecha = self.match(r'[a-zA-Z][a-zA-Z0-9]*')
                 if derecha:
                     resto_derecha = self.match(r'[a-zA-Z0-9]*')
@@ -226,7 +251,6 @@ class ArgonParser:
                 return False
             
     def generar_mapa_posiciones(self):
-        # Esta función generará el mapa de posiciones hasta el punto actual
         return ' -> '.join([f'po{i}' for i in range(self.indice + 1)])
 
     def analizar(self):
@@ -242,12 +266,12 @@ class ArgonParser:
 
 class ArgonApp:
     def __init__(self, root):
+        self.grammar_label = tk.Label(root, text="Cuando presione validar verá un mensaje que afirme si fue correcta su entrada, si hay error se indicará. Entiéndase 'po' como posición")
         self.parser = ArgonParser('')
         self.text = tk.Text(root, width=40, height=10)
         self.text.pack(padx=10, pady=10)
         self.validate_button = tk.Button(root, text="Validar", command=self.validate)
         self.validate_button.pack(pady=5)
-        self.grammar_label = tk.Label(root, text="Cuando presione validar verá un mensaje que afirme si fue correcta su entrada, si hay error se indicará. Entiéndase 'po' como posición")
         self.grammar_label.pack(pady=10)
 
     def validate(self):
